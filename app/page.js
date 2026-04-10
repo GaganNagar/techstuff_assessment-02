f43654f0d5b794df26ff1cecf41b04f02fda7a5a
Gagan Nagar <gaganngr319@gmail.com>
2026-04-10 12:13:40 +0530

    home

--- app/page.js
+++ app/page.js
@@ -1,7 +1,3 @@
-// export default function Home() {
-//   return <h1>Root Layout Page</h1>;
-// }
-
 export default function Home() {
-  return <h1 className="text-3xl text-blue-500">Tailwind Working</h1>;
+  return <h1>Root Layout Page</h1>;
 }
